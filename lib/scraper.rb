dea82bd7f5ddc9a9a546de6435badca2129ac5c4
Ronald Stephens <rstephens@bluemorpho.cc>
2019-09-01 19:09:26 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ class Scraper
   html = open("http://flatironschool.com/")
   doc = Nokogiri::HTML(html)
   doc.css(".title-oE5vT4").text
-  puts doc.css(".title-oE5vT4").text
+  # puts doc.css(".title-oE5vT4").text
 
 
 
