48fc0a16ded2dd9c5a0567297c581774ec39402b
Ronald Stephens <rstephens@bluemorpho.cc>
2019-09-01 19:00:12 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,8 +4,8 @@ require 'open-uri'
 class Scraper
   doc = open("http://flatironschool.com/")
   doc = Nokogiri::HTML(open("http://flatironschool.com/"))
-  doc.css(".headline-260IBN").text
-  puts doc.css(".headline-260IBN").text
+  doc.css(".headline_260IBN").text
+  puts doc.css(".headline_260IBN").text
 
 
 
